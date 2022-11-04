2132cac673356c2c2952017eb1f7d7dbad609d19
Pavel <rut100500@yandex.ru>
2022-11-04 16:54:43 +0300

    Домашнее задание задача 13

---
+++ task2_2/Program.cs
@@ -0,0 +1,45 @@
+﻿//  Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.WriteLine("Введите трехзначное натуральное число");
+int enter = Convert.ToInt32(Console.ReadLine());
+if(enter < 100)
+{
+    Console.WriteLine("Ошибка! Число не сооотвествует требованиям!");
+}
+else
+    {
+    int third = GetThirdNum(enter);
+    Console.WriteLine($"Третья цифра = {third}");
+    }
+
+
+int GetLengtNum(int num)
+{
+    int count = 0;
+    while(num > 0)
+    {
+        num /= 10;
+        count++;
+    }
+    return count;
+
+}
+
+int GetThirdNum(int input)
+{
+    int lenght = GetLengtNum(input);
+    int[] array = new int[lenght];
+
+    for (int i = lenght - 1; i > 0; i--)
+    {
+        array[i] = input % 10;
+        input /= 10;  
+    }
+    // int none = (array[0]);
+    // Console.WriteLine($"Первая цифра = {none}");
+    
+    int res = (array[2]);
+    return res;
+    
+}
+
